3045eb9dcba10ad22b14de78cd43fc8ac4b79134
Yutsss <adyuta123@gmail.com>
2025-01-04 04:09:50 +0700

    remove duplicates from sorted array II

---
+++ medium/80-remove-duplicates-from-sorted-array-II.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+  int removeDuplicates(vector<int> &nums)
+  {
+    if (nums.size() == 1 || nums.size() == 2)
+    {
+      return nums.size();
+    }
+    int counter = 0;
+    int checker = 0;
+    int index = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if ((i != 0) && nums[i - 1] != nums[i])
+      {
+        checker = 0;
+      }
+      checker++;
+      if (checker <= 2)
+      {
+        nums[index] = nums[i];
+        if (i != nums.size() - 1)
+        {
+          index++;
+        }
+        counter++;
+      }
+    }
+    return counter;
+  }
+};
