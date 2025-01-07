deace7324056d4e1efbc4d542f02023689bb02a8
Yutsss <adyuta123@gmail.com>
2025-01-07 17:19:04 +0700

    department highest salary

---
+++ sql/medium/184-department-highest-salary.sql
@@ -0,0 +1,12 @@
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d ON (e.departmentId = d.id)
+WHERE
+    e.salary = (
+        SELECT MAX(e1.salary)
+        FROM Employee e1
+        WHERE e1.departmentId = e.departmentId
+    )
